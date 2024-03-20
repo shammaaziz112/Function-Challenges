99f968df255144374de54f07252ca6baf78f6463
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-03-20 15:06:03 +0300

    edit Challenge 1 and finished Challenge 3

--- src/Program.cs
+++ src/Program.cs
@@ -5,12 +5,81 @@ namespace FunctionChallenges
 {
     class Program
     {
-        static void StringNumberProcessor(string name1, int num1, int num2, string name2)
+        static string StringNumberProcessor(params object[] Value)
         {
-            Console.WriteLine($"{name1} {name2}; {num1 + num2}");
+            string sentence = "";
+            int number = 0;
+
+            foreach (var item in Value)
+            {
+                if (item is int num)
+                {
+                    number += num;
+                }
+                else if (item is string str)
+                {
+                    sentence += $"{str} ";
+                }
+            }
+            return $"{sentence}{number}";
         }
 
         static void SwapObjects() { }
+
+        static void GuessingGame(int range = 0)
+        {
+            Random randomNumber = new Random();
+            if (range == 0)
+            {
+                range = randomNumber.Next(1, 100);
+            }
+            int num = randomNumber.Next(range);
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Guess a number?");
+                    int input = Convert.ToInt32(Console.ReadLine());
+
+                    if (input == num)
+                    {
+                        Console.WriteLine("You Guess it!!🎉");
+                        break;
+                    }
+                    else if (input >= num - 2 && input <= num + 2)
+                    {
+                        Console.WriteLine("You are too close 🤩");
+
+                    }
+                    else if (input >= num - 5 && input <= num + 5)
+                    {
+                        Console.WriteLine("You are close 🫢");
+
+                    }
+                    else if (input >= num - 10 && input <= num + 10)
+                    {
+                        Console.WriteLine("You are far 😥");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are too far 🫠");
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Enter a number");
+
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
 
         static string ReverseWords(string str)
         {//enter wrong commit and i couldn't know how to change it
@@ -21,14 +90,15 @@ namespace FunctionChallenges
             }
             string[] strArray = reversed.ToString().Split(" ");
             Array.Reverse(strArray);
-            str = String.Join(' ', strArray );
+            str = String.Join(' ', strArray);
             return str;
         }
         static void Main(string[] args)
         {
             // Challenge 1: String and Number Processor
             Console.WriteLine("Challenge 1: String and Number Processor");
-            StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
+            Console.WriteLine(StringNumberProcessor("Hello", 100, 200, "World"));
+            // Expected outcome: "Hello World; 300"
 
             // Challenge 2: Object Swapper
             // Console.WriteLine("\nChallenge 2: Object Swapper");
@@ -50,10 +120,25 @@ namespace FunctionChallenges
             // Console.WriteLine($"Strings: {str1}, {str2}");
 
             // Challenge 3: Guessing Game
-            // Console.WriteLine("\nChallenge 3: Guessing Game");
-            // Uncomment to test the GuessingGame method
-            // GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            Console.WriteLine("\nChallenge 3: Guessing Game");
+            try
+            {
+                Console.WriteLine("Enter number range: ");
+                int numberRange = int.Parse(Console.ReadLine());
+                // Uncomment to test the GuessingGame method
+                GuessingGame(numberRange); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("You didn't enter number range so it will be random 😛");
+                GuessingGame();
+            }
+            catch (System.Exception)
+            {
 
+                throw;
+            }
+            
             // Challenge 4: Simple Word Reversal
             Console.WriteLine("\nChallenge 4: Simple Word Reversal");
             string sentence = "This is the original sentence!";
